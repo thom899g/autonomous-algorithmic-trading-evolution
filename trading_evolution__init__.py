8706cf4abc7932227fca57bf637f3b5b73273a9b
thom899g <153515950+thom899g@users.noreply.github.com>
2026-03-03 08:07:19 +0100

    Automated commit by HiveMind

---
+++ trading_evolution__init__.py
@@ -0,0 +1,7 @@
+"""
+Autonomous Algorithmic Trading Evolution System
+Core package for autonomous trading strategy discovery and evolution
+"""
+
+__version__ = "1.0.0"
+__author__ = "Evolution Ecosystem"
